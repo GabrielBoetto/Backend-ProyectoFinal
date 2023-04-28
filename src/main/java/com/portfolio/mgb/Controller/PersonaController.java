ab31cb0eda9fe6e86bb1684275abedbaa5d553be
Gabriel <gabrielboetto@hotmail.com>
2023-04-28 04:51:41 -0300

    comentamos para probar

--- src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/personas")
+//@RequestMapping("/personas")
 @CrossOrigin(origins = "https://frantendgb.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
