b474c2836792996bd0efb08710bd22398c3e39df
Gabriel <gabrielboetto@hotmail.com>
2023-05-01 20:21:09 -0300

    cambios en barras /

--- src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://frantendgb.web.app/")
+@CrossOrigin(origins = "https://frantendgb.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
