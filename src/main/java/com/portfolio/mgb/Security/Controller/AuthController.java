9e3238551774e764d6b9038b7f57426b93b2db0a
Gabriel <gabrielboetto@hotmail.com>
2023-05-01 18:01:26 -0300

    build de proyecto

--- src/main/java/com/portfolio/mgb/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/mgb/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://mgbfrontend.web.app"})
+@CrossOrigin(origins = {"https://frantendgb.web.app/"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
